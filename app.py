597ede218dcd7e7b4ec4bf4e309680d7d87ef238
faire21 <31628379+faire21@users.noreply.github.com>
2020-04-26 15:08:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from PIL import Image
 from resizeimage import resizeimage
 import sys
+import boto3
+import io
 
 with open(sys.argv[1],'r+b') as f:
-    with Image.open(f) as image:
-        cover = resizeimage.resize_cover(image,[150,150])
-        cover.save(sys.argv[2],image.format)
+    filename = 'SMALL'+f.name
+    s3 = boto3.client('s3')
+    s3r = boto3.resource('s3')
+    bucket = 'practica2buck'
+    s3.download_file(bucket, f.name, filename)
+    print('image downloaded')
 
+with Image.open(filename) as image:
+    cover = resizeimage.resize_cover(image,[150,150])
+
+memfile = io.BytesIO()
+cover.save(memfile, format = cover.format)
+
+memfile.seek(0)
+
+s3r.Bucket(bucket).put_object(Key=filename, Body=memfile)
