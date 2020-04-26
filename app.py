6ea57ee84db20f859e2027b8c7e2ddcb72fde656
faire21 <31628379+faire21@users.noreply.github.com>
2020-04-26 16:45:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,20 +4,26 @@ import sys
 import boto3
 import io
 
-with open(sys.argv[1],'r+b') as f:
-    filename = 'SMALL'+f.name
-    s3 = boto3.client('s3')
-    s3r = boto3.resource('s3')
-    bucket = 'practica2buck'
-    s3.download_file(bucket, f.name, filename)
-    print('image downloaded')
+#Open up aws 
+s3 = boto3.client('s3')
+s3r = boto3.resource('s3')
+bucket = 'practica2buck'
 
+#New name for the new file
+filename = 'SMALL'+sys.argv[1]
+
+#Download the file with the new name
+s3.download_file(bucket, sys.argv[1], filename)
+print('image downloaded')
+
+#Open downloaded file and resize image
 with Image.open(filename) as image:
     cover = resizeimage.resize_cover(image,[150,150])
 
+#Convert Pil image to a file that aws can store
 memfile = io.BytesIO()
 cover.save(memfile, format = cover.format)
-
 memfile.seek(0)
 
+#AWS PUT
 s3r.Bucket(bucket).put_object(Key=filename, Body=memfile)
